2fd60ab0f1e096050c492b118560316702a803bf
Bondo <bachosofromadze@gmail.com>
2024-03-11 18:40:02 +0400

    final commit before adding lerp

--- scripts/player/character.gd
+++ scripts/player/character.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-
+var direction = Vector3.ZERO
 
 @export var walkSpeed = 5
 @export var jumpVelocity = 4
@@ -18,15 +18,14 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(event.relative.x * mouseSensitivity * -1))
 		head.rotate_x(deg_to_rad(event.relative.y * mouseSensitivity * -1))
-		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-85), deg_to_rad(85))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-80), deg_to_rad(85))
 
 func _physics_process(delta):
 	applyGravity(delta)
 	jump()
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	walk(direction)
-
 	move_and_slide()
 
 func applyGravity(delta):
@@ -35,10 +34,10 @@ func applyGravity(delta):
 func jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jumpVelocity
-func walk(direction):
-	if direction:
-		velocity.x = direction.x * walkSpeed
-		velocity.z = direction.z * walkSpeed
+func walk(dir):
+	if dir:
+		velocity.x = dir.x * walkSpeed
+		velocity.z = dir.z * walkSpeed
 	else:
 		velocity.x = move_toward(velocity.x, 0, walkSpeed)
 		velocity.z = move_toward(velocity.z, 0, walkSpeed)
