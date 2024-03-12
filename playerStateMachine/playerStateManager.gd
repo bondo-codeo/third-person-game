417b8e4314b3f7edd8b36fdd69fb5063c2dcb574
Bondo <bachosofromadze@gmail.com>
2024-03-13 00:01:58 +0400

    added keymaps for crouch and run

--- playerStateMachine/playerStateManager.gd
+++ playerStateMachine/playerStateManager.gd
@@ -1,11 +1,15 @@
 extends Node
 
+enum groundStates {idle, walking, running, crouching}
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("forward"):
+		print("walking")
+	else:
+		print("idle")
