505784a719e7a19299b65d87c2f258f5142f7e81
Bondo <bachosofromadze@gmail.com>
2024-04-09 19:08:45 +0400

    removed map, added in a new placeholder place

--- testing stuff/bullet.gd
+++ testing stuff/bullet.gd
@@ -2,12 +2,16 @@ extends Node3D
 
 var speed = 90
 
-@onready var mesh_instance_3d = $MeshInstance3D
+@onready var mesh = $MeshInstance3D
 
-@onready var ray_cast_3d = $RayCast3D
+@onready var ray = $RayCast3D
 
+@onready var particle = $GPUParticles3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += transform.basis * Vector3(0, 0, -speed) * delta
-	
+	if ray.is_colliding():
+		mesh.visible = false
+		particle.emitting = true
+		print("bullet hit something baby")
